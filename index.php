e54b34e15f741dbe17d7f155e721c8a055e2cb03
Robson <robsonsuzin@hotmail.com>
2020-03-15 16:44:03 -0300

    Initial

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php echo 'teste';?>
+<iframe name="iframevideo" width="560" height="315"
+        src="https://player.vimeo.com/video/397718363"
+        frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<script>
+    window.setInterval(function () {
+        meuIframe = document.querySelector("iframe");
+        conteudoIframe= meuIframe.contentDocument || meuIframe.contentWindow.document;
+        video = conteudoIframe.querySelector(".focus-target");
+        console.log(video);
+    }, 1000);
+
+</script>
